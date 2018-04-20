7862dde894d2af1c507bfaa6d50f308f875efb32
Noel Ian <noel.simborios@gmail.com>
2018-04-20 10:37:12 +0800

    Optical View Partial

--- src/views/optical/OpticalView.js
+++ src/views/optical/OpticalView.js
@@ -1,17 +1,28 @@
 import React from 'react'
-import BaseMVPView from '../common/base/BaseMVPView'
+import BaseView from '../common/base/BaseView'
 import { GenericTextBox } from '../../ub-components/TextBox/'
+import Uploader from '../../ub-components/FileUploader/Uploader'
 
 
-class OpticalView extends BaseMVPView {
+class OpticalView extends BaseView {
     render () {
         return (
 
             <div>
-                <GenericTextBox
-                placeholder = { 'Amount' }
-                type = { 'text' }/>
-            </div>
+             <form>
+             <GenericTextBox
+              placeholder = { 'Amount' }
+              type = { 'text' }/>
+             <Uploader/>
+             <Uploader/>
+
+             <input type="submit" value="submit"/>
+
+
+
+         </form>
+             </div>
+
 
 
         )
