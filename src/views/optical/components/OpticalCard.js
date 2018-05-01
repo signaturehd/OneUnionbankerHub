e3e3a702a3123c5c4e8d107f61a1fca4ad6a323e
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-05-01 12:19:29 +0800

    updated optical reimbursement

--- src/views/optical/components/OpticalCard.js
+++ src/views/optical/components/OpticalCard.js
@@ -10,35 +10,89 @@ import Modal from '../modal/OpticalReviewModal'
     super(props)
     this.state = {
       showConfirmation: false,
+      file: '',
+      file2: '',
+      imagePreviewUrl: '',
+      imagePreviewUrl2: ''
     }
+    this._handleImageChange = this._handleImageChange.bind(this)
+    this._handleImageChange2 = this._handleImageChange2.bind(this)
+    this._handleSubmit = this._handleSubmit.bind(this)
   }
+  _handleSubmit(e) {
+      e.preventDefault()
+      // TODO: do something with -> this.state.file
+    }
+
+    _handleImageChange(e) {
+      e.preventDefault()
+
+      let reader = new FileReader()
+      let file = e.target.files[0]
+
+      reader.onloadend = () => {
+        this.setState({
+          file: file,
+          imagePreviewUrl: reader.result
+        })
+      }
+
+      reader.readAsDataURL(file)
+    }
+    _handleImageChange2(e1) {
+      e1.preventDefault()
+      let reader2 = new FileReader()
+      let file2 = e1.target.files[0]
+
+      reader2.onloadend = () => {
+        this.setState({
+          file2: file2,
+          imagePreviewUrl2: reader2.result
+        })
+      }
+      reader2.readAsDataURL(file2)
+    }
 
   render () {
     const { proceedModal } = this.props
     const { showConfirmation, showModal, confirm, cancel } = this.state
+    let {imagePreviewUrl, imagePreviewUrl2} = this.state;
+    let $imagePreview = null;
+    let $imagePreview2 = null;
+      $imagePreview = (<img className = {'optical-image-view'} src={imagePreviewUrl} />);
+      $imagePreview2 = (<img className = {'optical-image-view'} src={imagePreviewUrl2} />);
     return (
+
       <div className = { 'optical-card' } >
+        <form onSubmit={this._handleSubmit}>
           <div className = {'optical-header'} >
             <h5 >Form Attachments</h5>
               <div className = {'optical-body'}>
-                <TextBox/>
-                <TextBox/>
+                <div>
+                     <input type="file" onChange={this._handleImageChange} />
+                     <input type="file" onChange={this._handleImageChange2} />
+                     <button type="submit" onClick={this._handleSubmit}>Upload Image</button>
               </div>
-          </div>
+            </div>
+        </div>
           <div className = {'optical-footer-left'}>
             <div className = { 'optical-modal-review' }>
               <div className = { 'optical-image-view' }>
-                <div className = { 'optical-image-layer' }></div>
+                  {$imagePreview}
+                <div className = { 'optical-image-layer' }>
+                </div>
               </div>
               <div className = { 'optical-image-view' }>
+                  {$imagePreview2}
                 <div className = {  'optical-image-layer' }></div>
               </div>
             </div>
             <div className = { 'optical-button-submit' }>
-              <Button show = {() => this.setState({showConfirmation : true})} />
+              <Button onClick={this._handleSubmit} />
             </div>
           </div>
-      </div>
+        </form>
+    </div>
     )
   }
 }
