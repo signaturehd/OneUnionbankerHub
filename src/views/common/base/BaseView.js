85106b9fbd92e50293194bb9c3ced25052851a34
Carlo <cuevascarlo07@gmail.com>
2018-05-07 17:14:31 +0800

    Updated base view with notify component

--- src/views/common/base/BaseView.js
+++ src/views/common/base/BaseView.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 
 import Loader from '../../../ub-components/Loader'
+import Notify from '../../../ub-components/Notify'
 
 class BaseView extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      loader : false
+      loader : false,
+      showNotif : true,
+      title : null,
+      message : null,
+      type : null,
     }
     this.showLoading = this.showLoading.bind(this)
     this.hideLoading = this.hideLoading.bind(this)
@@ -20,10 +25,27 @@ class BaseView extends Component {
     this.setState({ loader : false })
   }
 
+  showNotify (title, message, type) {
+    this.setState({ showNotif : true, title, message, type })
+  }
+
+
   render () {
+
+    const { loader, showNotif, title, message, type } = this.state
+
     return (
       <div>
-        <Loader show = { this.state.loader }/>
+        <Loader show = { loader }/>
+        {
+          showNotif &&
+          <Notify
+            title = { title }
+            message = { message }
+            type = { type }
+            onClose = { () => this.setState({ showNotif : false }) }
+          />
+        }
       </div>
     )
   }
