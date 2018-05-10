0c6b16f5afdc9db96511a87addedd6e4ec21135e
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-05-10 23:15:27 +0800

    updated interactor

---
+++ src/domain/interactor/podcast/PodcastRecommendationInteractor.js
@@ -0,0 +1,10 @@
+export default class PodcastRecommendationInteractor
+{
+  constructor (client) {
+    this.client = client
+  }
+
+  execute () {
+    return this.client.getPodcastsRecommendations(this.client.getToken())
+  }
+}
