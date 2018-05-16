fb39d77e0c1e76d7d969e1741fd8b38028d55517
Noel Ian T. Simborios <noel.simborios@gmail.com>
2018-05-16 15:04:36 +0800

    /transactions:
    For Designing

--- src/views/transaction/modals/MplModalFragment/MplModalFragment.js
+++ src/views/transaction/modals/MplModalFragment/MplModalFragment.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Modal } from '../../../ub-components/'
 
-import './styles/transaction-modal.css'
+import '../styles/transaction-modal.css'
 
-class TransactionModal extends Component {
+class MplModalFragment extends Component {
   constructor (props) {
     super(props)
     this.state = {
@@ -20,14 +20,14 @@ class TransactionModal extends Component {
     const { onClose, transactionResponse } = this.props
     // TODO map the transaction details here
     console.log(transactionResponse)
+
     return (
       <Modal
         isDismisable = { true }
         onClose = { onClose }>
 
         <div>
-         
-         <h4> DateFiled : {transactionResponse && transactionResponse.dateFiled}</h4>
+        <h4> DateFiled : {transactionResponse && transactionResponse.dateFiled}</h4>
         <h4> Status: {transactionResponse && transactionResponse.status.name}</h4>
         <h4> Account No: {transactionResponse && transactionResponse.details.AccountNo}</h4>
         <h4> Reference No: {transactionResponse && transactionResponse.details.ReferenceNumber} </h4>
@@ -37,9 +37,9 @@ class TransactionModal extends Component {
         <h4> Term: {transactionResponse && transactionResponse.details.Term.Name} </h4>
         <h4> Rate: {transactionResponse && transactionResponse.details.Term.Rate}</h4>
         <h4> Duration: {transactionResponse && transactionResponse.details.Term.Term}</h4>
-        
-
-
+        <h4>Amortization : {transactionResponse && transactionResponse.details.Amortization}</h4>
+        <h4>Credit Ratio : {transactionResponse && transactionResponse.details.CreditRatio}</h4>
+        <h4>Notice & Promisory : {transactionResponse && transactionResponse.FormAgreements.Form}</h4>
 
         </div>
       </Modal>
@@ -51,4 +51,4 @@ TransactionModal.propTypes = {
   onClose: PropTypes.func,
 }
 
-export default TransactionModal
+export default MplModalFragment
---
+++ src/views/transaction/modals/OpticalModalFragment/OpticalModalFragment.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Modal } from '../../../ub-components/'
+
+import '../styles/transaction-modal.css'
+
+class OpticalModalFragment extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      transactionID: []
+    }
+  }
+
+ componentDidMount () {
+    this.props.presenter()
+ }
+
+ render () {
+    const { onClose, transactionResponse } = this.props
+    // TODO map the transaction details here
+    console.log(transactionResponse)
+    return (
+      <Modal
+        isDismisable = { true }
+        onClose = { onClose }>
+
+        <div>
+        <h4> DateFiled : {transactionResponse && transactionResponse.dateFiled}</h4>
+        <h4> Transaction Status: {transactionResponse && transactionResponse.status.name}</h4>
+        <h4> Account No: {transactionResponse && transactionResponse.details.AccountNo}</h4>
+        <h4> Reference No: {transactionResponse && transactionResponse.details.ReferenceNumber} </h4> 
+        <h4> Amount: {transactionResponse && transactionResponse.details.Amount}</h4>
+        <h4> {transactionResponse && transactionResponse.details.Attachments.FileName}> </h4>
+        <h4> {transactionResponse && transactionResponse.FormAgreements}</h4>
+
+        </div>
+      </Modal>
+    )
+  }
+}
+
+TransactionModal.propTypes = {
+  onClose: PropTypes.func,
+}
+
+export default OpticalModalFragment
