3bd90d17a87593849e1be166ef1e0969248a307e
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-05-18 22:05:59 +0800

    updated interactor for reviews

---
+++ src/domain/interactor/podcast/PodcastReviewInteractor.js
@@ -0,0 +1,10 @@
+export default class PodcastReviewInteractor
+{
+  constructor (client) {
+    this.client = client
+  }
+
+  execute () {
+    return this.client.getPodcastsReviews(this.client.getToken())
+  }
+}
