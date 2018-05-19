39aae6feca53dfc0612fd00f1a14ff37ab5caac0
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-05-19 19:55:27 +0800

    pull on dev

--- src/views/otp/OtpModal.js
+++ src/views/otp/OtpModal.js
@@ -4,7 +4,7 @@ import ConnectPartial from '../../utils/ConnectPartial'
 import BaseMVPView from '../common/base/BaseMVPView'
 import Presenter from './presenter/OtpPresenter'
 
-import { GenericTextBox, GenericButton, Modal } from '../../ub-components'
+import { GenericTextBox, GenericButton, Modal, CircularLoader } from '../../ub-components'
 
 class OtpModal extends BaseMVPView {
   constructor (props) {
@@ -12,29 +12,65 @@ class OtpModal extends BaseMVPView {
 
     this.state = {
       otp: '',
+      disableSubmit : false,
+      disableResend : false,
+      text : null
     }
+
+    this.onResendSuccess = this.onResendSuccess.bind(this)
   }
 
   onOtpSuccess () {
     // TODO redirect to login
   }
 
+  onResendSuccess () {
+    this.setState({ disabledResend : false, text : '' })
+  }
+
   render () {
     const { transactionType, username, onClose } = this.props
-    const { otp } = this.state
+    const { otp, text, disableSubmit, disableResend } = this.state
     return (
 
       <Modal
           onClose = {onClose}
         >
-        { super.render() }
-        <GenericTextBox text= "OTP"
-          placeholder = "OTP"
-          type = ""
-          onChange={ e => this.setState({ otp: e.target.value }) }  />
-          <br/>
-          <GenericButton text= "Submit" onClick={ () => this.presenter.verifyOtp(username, otp, transactionType) } />
-          <GenericButton text= "Resend OTP" onClick={ () => this.presenter.resendOtp(username, transactionType) } />
+        {
+          disableSubmit || disableResend ?
+          <center>
+            <h3>{ text }</h3>
+            <br/>
+            <br/>
+            <CircularLoader show={true}/>
+          </center>
+          :
+          <div>
+            <GenericTextBox text= "OTP"
+              placeholder = "OTP"
+              type = ""
+              onChange={ e => this.setState({ otp: e.target.value }) }
+            />
+            <br/>
+            <GenericButton text= "Submit"
+              onClick={ () => {
+                  this.presenter.verifyOtp(username, otp, transactionType),
+                  this.setState({ disableSubmit : true, text : 'Please wait while were verifying your OTP' })
+                }
+              }
+              disabled = {this.state.disableSubmit}
+             />
+            <GenericButton text= "Resend OTP"
+              onClick={ () => {
+                  this.presenter.resendOtp(username, transactionType),
+                  this.setState({ disableResend: true, text : 'Please wait while were resending your OTP' })
+                }
+              }
+              disabled = {this.state.disableResend}
+            />
+          </div>
+        }
+
       </Modal>
       )
   }
