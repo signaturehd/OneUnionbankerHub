aa5e2b653b5c16280a3f45095fe0a0db4ba81463
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-05-19 19:50:20 +0800

    updated podcast pull library on dev

--- src/views/library/LibraryFragment.js
+++ src/views/library/LibraryFragment.js
@@ -20,15 +20,24 @@ class LibraryFragment extends BaseMVPView {
       books : [],
       recommended : [],
       borrowed : [],
+      reserve: [],
+      filteredBook: [],
       showRating : false,
-      showBook : false
+      showBook : false,
+      showBorrowed : false,
+      showRecommendation : false,
+      showBorrowedFiltered : false,
+      searchString : '',
+
     }
+    this.updateSearch = this.updateSearch.bind(this)
   }
 
   componentDidMount () {
       this.presenter.getBooks()
       this.presenter.getBooksBorrowed()
       this.props.setSelectedNavigation(5)
+      this.props.history.push('/mylearning/books')
   }
 
   showBooks (books) {
@@ -46,9 +55,21 @@ class LibraryFragment extends BaseMVPView {
   navigate () {
     this.props.history.push ('/mylearning')
   }
-
+  updateSearch () {
+    this.setState({ searchString: this.refs.search.value.substr(0 , 20) })
+  }
+  showBorrowedFiltered (filteredBook) {
+    this.setState({ filteredBook })
+  }
   render () {
-    const { books, tabs, recommended, borrowed } = this.state
+    const { filteredBook, books, tabs, recommended, borrowed, reserve } = this.state
+    const { history } = this.props
+    let _books = this.state.books
+    const _borrowed = this.state.borrowed
+    const search = this.state.searchString.trim().toLowerCase()
+    if (search.length > 0) {
+      _books = _books.filter(books => books.title.toLowerCase().match(search))
+    }
     return (
       <div>
       { super.render() }
@@ -56,6 +77,12 @@ class LibraryFragment extends BaseMVPView {
         <i className = { 'back-arrow' } onClick = { this.navigate.bind(this) }></i>
         <h2 className = { 'header-margin-default' }>LIBRARY</h2>
       </div>
+      <input type = 'text'
+           className = {'booksSearchBar'}
+           ref='search'
+           placeholder = {'Search Books'}
+           value = { this.state.searchString }
+           onChange = { this.updateSearch } />
         <div className = { 'tabs-container' }>
           <input
             className = { 'input-tab' }
@@ -63,20 +90,20 @@ class LibraryFragment extends BaseMVPView {
             type='radio'
             name='tabs'
             defaultChecked />
-          <label htmlFor = 'tab1'>All Books</label>
+          <label  className = { 'mobile-icon' } htmlFor = 'tab1'>All Books</label>
 
           <input
             className = { 'input-tab' }
             id='tab2'
             type='radio'
             name='tabs' />
-          <label htmlFor='tab2'>Recommended</label>
+          <label className = { 'mobile-icon' } htmlFor='tab2'>Recommended</label>
 
           <input className = { 'input-tab' } id='tab3'  type='radio' name='tabs' />
-          <label htmlFor = 'tab3' >Borrowed</label>
+          <label className = { 'mobile-icon' } htmlFor = 'tab3' >Borrowed</label>
 
           <section id='content1'>
-            <BookListFragment presenter={ this.presenter } books = { books } />
+            <BookListFragment presenter={ this.presenter } _books = { _books } />
           </section>
           <section id='content2'>
             <BookRecommendationFragment presenter = { this.presenter } recommended = { recommended } />
