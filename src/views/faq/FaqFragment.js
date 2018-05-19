52dfce8bfb8d3bb8b340986c4c9e6618c9728795
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-05-19 19:51:19 +0800

    faqs pull on dev

--- src/views/faq/FaqFragment.js
+++ src/views/faq/FaqFragment.js
@@ -17,40 +17,69 @@ class FaqFragment extends BaseMVPView {
     super(props)
     this.state = {
         faqs: [],
+        faqsCategory: [],
+        showCategories: [],
         show : false,
+        showFaqs : false,
+        faqsCategories : false,
+        showFaqsCategories : false,
+        searchString : ''
     }
+    this.updateSearch = this.updateSearch.bind(this)
+  }
+
+  updateSearch () {
+    this.setState({ searchString: this.refs.search.value.substr(0 , 20) })
   }
 
   componentDidMount () {
     this.presenter.getFaqs()
+    this.presenter.getFaqsCategories()
     this.props.setSelectedNavigation(3)
   }
 
-  faqs (faqs) {
+  showFaqs (faqs) {
     this.setState({ faqs })
+  }
+  faqsCategories (faqsCategory) {
+    this.setState({ faqsCategory })
   }
 
   render () {
-    const { faqs, show, details } = this.state
+    const { faqs, faqsCategory, show, details, showCategories } = this.state
+    let categorize = this.state.faqsCategory
+    const search = this.state.searchString.trim().toLowerCase()
+    if (search.length > 0) {
+      categorize = categorize.filter(faqs => (faqs.category.category).toLowerCase().match(search))
+    }
+
     return (
       <div className = {'container'}>
         { super.render() }
-        <h1>Faqs</h1>
+        <h1 className = { 'title-view' }>FAQs</h1>
+          <input type = 'text'
+                 className = 'newsSearchBar'
+                 ref="search"
+                 placeholder = {'Search Faqs'}
+                 value = { this.state.searchString }
+                 onChange = { this.updateSearch } />
+
+        {
+          show &&
+          <FaqModal onClose = { () => this.setState({ show: false })}  details = { details } />
+        }
         <div className = {'card-container'}>
         {
-        faqs.map((faqs, i) =>
+        categorize.map((faq, i) =>
           <FaqCardComponent
             key = {i}
-            faqs = { faqs }
+            categorize = { faq }
             onClick = { details => {
               this.setState({ details, show: true })
             }} />)
         }
         </div>
-        {
-          show &&
-          <FaqModal onClose = { () => this.setState({ show: false })} details = { details } />
-        }
+
       </div>
     )
   }
