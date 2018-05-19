127ec8e91d9f4670f33564cac799aa4a87542bee
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-05-19 22:36:43 +0800

    updated news pull on dev

--- src/views/navigation/NavigationView.js
+++ src/views/navigation/NavigationView.js
@@ -66,7 +66,7 @@ class NavigationView extends BaseMVPView {
     this.presenter.logout()
   }
   render () {
-    const { displayShow, displayNavIcon, displayNavIconState, selected } = this.state
+    const { displayShow, displayNavIcon, displayNavIconState, selected, onClick } = this.state
     const style = {
       show: {
           display : displayShow
@@ -76,7 +76,7 @@ class NavigationView extends BaseMVPView {
         <div className = { 'body-div' }>
           <header className = { 'page-boundary page-boundary--fixed-top' }>
             <DrawerAppBar
-              logout = { this.callLogout }
+              onClick = { onClick }
               displayNavIcon = { displayNavIcon } displayShow = { displayShow }
               hide = { () => this.setState({ displayShow : 'block' })}
               show = { () => this.setState({ displayShow : 'none' })} />
