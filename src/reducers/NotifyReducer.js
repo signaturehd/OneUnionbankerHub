e65abd885692a7125bb5c8c5c794ce131198d903
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-05-20 21:27:16 +0800

    reducers pull on dev

--- src/reducers/NotifyReducer.js
+++ src/reducers/NotifyReducer.js
@@ -4,7 +4,7 @@ import initialState from './initialState'
 export default function reducer (state = initialState.notify, action) {
   switch (action.type) {
     case ADD_NOTIFY:
-      return [ ...state, action.notifyObj ]
+      return [...state, action.notifyObj]
     case REMOVE_NOTIFY:
       return [
         ...state.slice(0, action.index),
