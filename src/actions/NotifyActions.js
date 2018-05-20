f50395d3c08e9f07a590d1567e71f97a8aa5b4d8
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-05-20 21:27:37 +0800

    actions pull on dev

--- src/actions/NotifyActions.js
+++ src/actions/NotifyActions.js
@@ -1,5 +1,5 @@
 import { CHANGE_TOKEN_EVENT } from '../utils/actionUtil'
 
-export function addNotify ( title, message, type, duration ) {
+export function addNotify (title, message, type, duration) {
   return { type: ADD_NOTIFY, title, message, type, duration }
 }
