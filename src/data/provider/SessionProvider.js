9df302dbb4755b18f20591e078e1120aa9951f96
Noel Ian T. Simborios <noel.simborios@gmail.com>
2018-05-21 03:21:44 +0800

    for fixing

--- src/data/provider/SessionProvider.js
+++ src/data/provider/SessionProvider.js
@@ -13,7 +13,8 @@ export default class SessionProvider {
   }
 
   getToken () {
-    return this.storage.getItem(TOKEN) || ''
+   // return this.storage.getItem(TOKEN) || ''
+   return '1b77067baf1dc815d80163dfe048888e5dded063da23568000d41b6bc9e14f22afdf47ee460fdfdb728f1c9c82d3ab09565f55faccb1b81782cb456df88f34b3'
   }
 
   setAccountToken (accountToken = '') {
@@ -22,7 +23,8 @@ export default class SessionProvider {
   }
 
   getAccountToken () {
-    return this.storage.getItem(ACCOUNT_TOKEN) || ''
+   // return this.storage.getItem(ACCOUNT_TOKEN) || ''
+   return 'rY5CFD9ja3as2I/pjQFneOlcHUapFQd/QTQ9pvZ5Z1H6dz7vR+v1D7KNZiUttVGA5mSMqRRUXB27kJEXlY26nBC1M+jNQGNtdmv64ukdRwmnfp3SAFCwvfaOXnmnKHY3GOcy0mnDR+zyBqzZt5ee54RdByw21a4='
   }
 
   setAccountNumber (accountNumber = '') {
