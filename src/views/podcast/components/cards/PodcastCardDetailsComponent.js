c201151f225a3a615009deaa094464ba80f40ed9
Carlo <cuevascarlo07@gmail.com>
2018-05-21 14:29:50 +0800

    Added missing cards

---
+++ src/views/podcast/components/cards/PodcastCardDetailsComponent.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Card }  from '../../../../ub-components/'
+
+import { MdStarOutline, MdStar } from 'react-icons/lib/md'
+import { FaPlayCircleO } from 'react-icons/lib/fa/'
+import Rating from 'react-rating'
+import './styles/podcast-component.css'
+
+class PodcastCardDetailsComponent extends Component {
+  constructor (props) {
+    super(props)
+     this.state = {
+      rating : 0,
+      imageUrl : ''
+    }
+  }
+  addRating (id, rating) {
+    this.props.presenter.rateBook(id, rating)
+  }
+
+  render () {
+    const { detail, podcasts, onClick, rateBook, history } = this.props
+    const { rating  } = this.state
+    const style = {
+      styles : {
+        background: `url(${podcasts.image}) rgba(0,0,0,0.7)`,
+        backgroundSize: 'cover',
+        backgroundBlendMode: 'color',
+        height: '117px',
+        margin: 'auto',
+      },
+      titleStyle : {
+        color : '#ffffff',
+        fontWeight: 'bold',
+        margin: 'auto',
+      },
+      authorStyle : {
+        color : '#ffffff',
+        fontSize : '12px',
+        float : 'right',
+        margin : 'auto',
+      }
+    }
+    return (
+        <Card className = { 'podcast-card-container' } >
+          <div style = {style.styles}
+               className = {'podcasts-details-side-right'}>
+              <h2 style = { style.titleStyle }> { podcasts.title } </h2>
+              <h2 style = { style.authorStyle }> -{podcasts.speaker} </h2>
+          <center>
+            <FaPlayCircleO
+              className = { 'fa-play-button-details' }
+              onClick = { onClick }/>
+          </center>
+          </div>
+          <div className = {'card-footer'}>
+          <center>
+           <Rating
+              rateBook = { (id, rating) => this.addRating(id, rating) }
+              emptySymbol = {<MdStarOutline style={{ fontSize: 25, color : '#c65e11' }} />}
+              fullSymbol = {<MdStar style={{ fontSize: 25,  color : '#c65e11' }} />}
+              onChange = { e => {
+                rateBook(detail.id, e)
+                this.setState({ rating : e })
+              }}
+              readonly
+              fractions = { 2 }
+              initialRating = { rating ? rating : podcasts.rating } />
+            </center>
+          </div>
+        </Card>
+    )
+  }
+}
+
+PodcastCardDetailsComponent.propTypes = {
+
+  onClick : PropTypes.func,
+  rateBook : PropTypes.func,
+
+}
+
+PodcastCardDetailsComponent.defaultProps = {
+
+}
+
+export default PodcastCardDetailsComponent
