c1a49225dc845bed405a10028faab70b1ecf9b19
tentenponce <poncetenten10@gmail.com>
2018-05-21 15:41:45 +0800

    Cleanup

--- src/views/podcast/PodcastView.js
+++ src/views/podcast/PodcastView.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import BaseMVPView from '../common/base/BaseMVPView'
 import Presenter from './presenter/PodcastPresenter'
 
-import ConnectPartial from '../../utils/ConnectPartial'
+import ConnectView from '../../utils/ConnectView'
 
 import './styles/podcast.css'
 import PodcastInteractor from '../../domain/interactor/podcast/PodcastInteractor'
@@ -67,9 +67,9 @@ class PodcastView extends BaseMVPView {
   }
 
   navigate () {
-    this.props.history.push ('/mylearning')
+    this.props.history.push('/mylearning')
   }
-  
+
   render () {
     const {
       podcasts,
@@ -81,94 +81,94 @@ class PodcastView extends BaseMVPView {
       disabled,
       detail,
       searchString,
-      selectedPodcast
+      selectedPodcast,
     } = this.state
     const { history } = this.props
-    let searchPodcast = this.state.podcasts
+
+    let searchPodcast = podcasts
     const search = searchString.trim().toLowerCase()
 
     if (search.length > 0) {
         searchPodcast = searchPodcast.filter(podcasts => podcasts.title.toLowerCase().match(search))
     }
-    const PodcastPlayer = () => (
-        <PodcastPlayerFragment
-            changeSelectedPodcast={ podcast => this.setState({ selectedPodcast: podcast }) }
-            selectedPodcast = { selectedPodcast }
-            presenter = { this.presenter }
-            podcasts = { podcasts }
-            podcastreview = { podcastreview }
-            history = { history }/>
-    )
 
     const PodCast = () => (
-    <div>
-    { super.render() }
-    <div className={ 'header-margin-container' }>
-      <i className = { 'back-arrow' } onClick = { this.navigate.bind(this) }></i>
-      <h2 className = { 'header-margin-default' }>PODCAST</h2>
-    </div>
-    <input type = 'text'
-             className = {'podcastsSearchBar'}
-             placeholder = {'Search Podcasts'}
-             value = { searchString }
-             onChange = { this.updateSearch } />
-    <div className = { 'tabs-container' }>
-      <input
-        className = { 'input-tab' }
-        id='tab1'
-        type='radio'
-        name='tabs'
-        defaultChecked />
-      <label htmlFor = 'tab1'>All Podcasts</label>
+      <div>
+        <div className={ 'header-margin-container' }>
+          <i className = { 'back-arrow' } onClick = { this.navigate.bind(this) }></i>
+          <h2 className = { 'header-margin-default' }>PODCAST</h2>
+        </div>
+        <input type = 'text'
+                 className = {'podcastsSearchBar'}
+                 placeholder = {'Search Podcasts'}
+                 value = { searchString }
+                 onChange = { this.updateSearch } />
+        <div className = { 'tabs-container' }>
+          <input
+            className = { 'input-tab' }
+            id='tab1'
+            type='radio'
+            name='tabs'
+            defaultChecked />
+          <label htmlFor = 'tab1'>All Podcasts</label>
 
-      <input
-        className = { 'input-tab' }
-        id='tab2'
-        type='radio'
-        name='tabs' />
-      <label htmlFor='tab2'>Recommended</label>
+          <input
+            className = { 'input-tab' }
+            id='tab2'
+            type='radio'
+            name='tabs' />
+          <label htmlFor='tab2'>Recommended</label>
 
-      <section id='content1'>
-        {
-          this.state.disabled ?
-          <center className = {'circular-loader-center'}>
-            <br/>
-            <CircularLoader show = { true }/>
-          </center>          :
-          <PodcastsListFragment
-            disabled = {this.state.disabled}
-            changeSelectedPodcast={ podcast => this.setState({ selectedPodcast: podcast }) }
-            presenter = { this.presenter }
-            searchPodcast = { searchPodcast }
-            details = { details }
-            history = { history } />
-        }
-      </section>
-      <section id='content2'>
-          <PodcastsRecommendationFragment
-            disabled = {this.state.disabled}
-            presenter = { this.presenter }
-            searchPodcast = { searchPodcast }
-            changeSelectedPodcast={ podcast => this.setState({ selectedPodcast: podcast }) }
-            podcastsRecommendation = { podcastsRecommendation }
-            history = { history } />
-      </section>
-    </div>
-  </div>
-  )
-  return (
-  <div>
-     <Switch>
-       <Route exact path = '/mylearning/podcast' render = { PodCast } />
-       <Route path = '/mylearning/podcast/player' render = { PodcastPlayer } />
-    </Switch>
-  </div>
-  )
-}
+          <section id='content1'>
+            {
+              this.state.disabled ?
+              <center className = {'circular-loader-center'}>
+                <br/>
+                <CircularLoader show = { true }/>
+              </center>          :
+              <PodcastsListFragment
+                disabled = { this.state.disabled }
+                changeSelectedPodcast={ podcast => this.setState({ selectedPodcast: podcast }) }
+                presenter = { this.presenter }
+                searchPodcast = { searchPodcast }
+                details = { details }
+                history = { history } />
+            }
+          </section>
+          <section id='content2'>
+              <PodcastsRecommendationFragment
+                disabled = { this.state.disabled }
+                presenter = { this.presenter }
+                searchPodcast = { searchPodcast }
+                changeSelectedPodcast={ podcast => this.setState({ selectedPodcast: podcast }) }
+                podcastsRecommendation = { podcastsRecommendation }
+                history = { history } />
+          </section>
+        </div>
+      </div>
+    )
+
+    return (
+      <div>
+        { super.render() }
+        <Switch>
+          <Route exact path = '/mylearning/podcast' render = { PodCast } />
+          <Route path = '/mylearning/podcast/player' render = { props =>
+            <PodcastPlayerFragment
+              { ...props }
+              changeSelectedPodcast={ podcast => this.setState({ selectedPodcast: podcast }) }
+              selectedPodcast = { selectedPodcast }
+              presenter = { this.presenter }
+              podcasts = { podcasts }
+              podcastreview = { podcastreview } /> } />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 PodcastView.propTypes = {
   setSelectedNavigation: PropTypes.func,
 }
 
-export default ConnectPartial(PodcastView, Presenter)
+export default ConnectView(PodcastView, Presenter)
