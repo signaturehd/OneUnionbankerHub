678d44929ef7c48b6e61905c358e1dd8f9f9eb88
tentenponce <poncetenten10@gmail.com>
2018-05-21 14:39:29 +0800

    Cleanup

--- src/views/common/components/Countdown/Countdown.js
+++ src/views/common/components/Countdown/Countdown.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+
 import moment from 'moment'
 
-
 class Countdown extends Component {
+
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       now: moment(),
       dateTo: moment(this.props.dateTo),
@@ -13,15 +14,19 @@ class Countdown extends Component {
       hour: 0,
       min: 0,
       sec: 0
-    };
-    this.timer = this.timer.bind(this);
+    }
+
+    this.timer = this.timer.bind(this)
   }
+
   componentDidMount() {
-    this.countdown = setInterval(this.timer, 1000);
+    this.countdown = setInterval(this.timer, 1000)
   }
+
   componentWillUnmount() {
-    clearInterval(this.countdown);
+    clearInterval(this.countdown)
   }
+
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.dateTo !== this.props.dateTo) {
       this.setState({
@@ -29,40 +34,47 @@ class Countdown extends Component {
       })
     }
   }
+
   timer() {
-    var now = moment();
-    var sec = this.state.dateTo.diff(now, "seconds") % 60;
-    var min = this.state.dateTo.diff(now, "minutes") % 60;
-    var hour = this.state.dateTo.diff(now, "hours") % 24;
-    var day = this.state.dateTo.diff(now, "days");
+    var now = moment()
+    var sec = this.state.dateTo.diff(now, 'seconds') % 60
+    var min = this.state.dateTo.diff(now, 'minutes') % 60
+    var hour = this.state.dateTo.diff(now, 'hours') % 24
+    var day = this.state.dateTo.diff(now, 'days')
+
     this.setState({
       now: now,
-      day: day < 10 ? "0" + day : day,
-      hour: hour < 10 ? "0" + hour : hour,
-      min: min < 10 ? "0" + min : min,
-      sec: sec < 10 ? "0" + sec : sec
-    });
+      day: day < 10 ? '0' + day : day,
+      hour: hour < 10 ? '0' + hour : hour,
+      min: min < 10 ? '0' + min : min,
+      sec: sec < 10 ? '0' + sec : sec,
+    })
   }
+
   renderDays() {
     if (this.state.day > 1) {
-      return this.state.day + " days ";
+      return this.state.day + ' days ';
     } else if (this.state.day === 1) {
-      return this.state.day + " day ";
+      return this.state.day + ' day ';
     } else {
-      return "";
+      return '';
     }
   }
+
   render() {
+    const { hour, min, sec } = this.state
+
     return (
       <div>
-          {this.renderDays()} {this.state.hour}:{this.state.min}:{this.state.sec}
+          { this.renderDays() }
+          { `${hour}:${min}:${sec}` }
       </div>
-    );
+    )
   }
 }
 
 Countdown.defaultProps = {
-  'dateTo': { year: 2058, month: 11, date: 25 }
+  dateTo: { year: 2058, month: 11, date: 25 }
 }
 
 export default Countdown
