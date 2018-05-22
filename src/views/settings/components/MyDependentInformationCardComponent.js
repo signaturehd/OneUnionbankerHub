03fb53deb78b0fe8193aea4c4d0acc9cb106a47d
Noel Ian T. Simborios <noel.simborios@gmail.com>
2018-05-22 16:05:50 +0800

    Aligned information with mobile

---
+++ src/views/settings/components/MyDependentInformationCardComponent.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import { Card } from '../../../ub-components/'
+
+import './styles.css'
+
+class MyDependentInformationCardComponent extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    const { profile, onClick, rank , linemanager } = this.props
+    return (
+         <div className = 'card-container'>
+            {
+                profile.dependent &&
+                profile.dependent.map((dependents, i) => ((
+                
+
+              <h5 className = "title"> DEPENDENTS: { dependents.dependent }</h5>
+
+
+                ))
+              
+            
+
+}
+
+  
+        
+          </div>
+    )
+  }
+}
+
+MyDependentInformationCardComponent.propTypes = {
+  onClick : PropTypes.func,
+}
+
+export default MyDependentInformationCardComponent
