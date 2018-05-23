7c73cfdbd3edff15dadb1c9384d23ff537703aa5
Noel Ian Simborios <noel.simborios@gmail.com>
2018-05-23 04:29:50 +0000

    SessionProvider.js edited online with Bitbucket

--- src/data/provider/SessionProvider.js
+++ src/data/provider/SessionProvider.js
@@ -13,8 +13,7 @@ export default class SessionProvider {
   }
 
   getToken () {
-    //eturn this.storage.getItem(TOKEN) || ''
-    return '58d557193e52858e5ffd598fb4d0a0cf96705f0434cc4c6fc64922a296e4474c0cf8a6c2a58e85ab4c42e7971bb878b85c10c52358e4ce4b5b02882cd8edd6a7'
+    return this.storage.getItem(TOKEN) || ''
 
   }
 
@@ -23,8 +22,7 @@ export default class SessionProvider {
   }
 
   getAccountToken () {
-    //return this.storage.getItem(ACCOUNT_TOKEN) || ''
-    return 'rY5CFD9ja3as2I/pjQFmcIFZOE8P5PiCEnh+xQbcBDdvrWLTbeslRIRj0FLWA97aDvLos+M5mL6Q7HJb0H2EYXCFpVtWEjpkiShED8aly5M6tEMmTz69e7Y23WPPARozoJJJoqtO7pJwNMuBZuS/D3qIxY5IPcs='
+    return this.storage.getItem(ACCOUNT_TOKEN) || ''
 
 
   }
