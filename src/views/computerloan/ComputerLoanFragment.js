a0537c4086988ebc2427e7bf4ee87266ae2a2b95
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-06-19 14:20:04 +0800

    WIP computerloan

--- src/views/computerloan/ComputerLoanFragment.js
+++ src/views/computerloan/ComputerLoanFragment.js
@@ -5,18 +5,71 @@ import BaseMVPView from '../common/base/BaseMVPView'
 import Presenter from './presenter/MplComputerLoanPresenter'
 import ConnectView from '../../utils/ConnectView'
 
-import FormComponent from './components/MplComputerLoanCardComponent'
+import { CircularLoader } from '../../ub-components/'
+
+
+import FormComponent from './components/MplFormCardComponent'
 
 class ComputerLoanFragment extends BaseMVPView {
   constructor (props) {
     super(props)
     this.state = {
+      purposeOfAvailment: [],
+      selectedPoa: '',
+      formAttachments: [],
+      loanType: 3,
+      validateLoanType : [],
+      offset : [],
+      enabledLoader : false,
+      noticeResponse : null, /* notice response*/
+      showNoticeResponseModal : false,
+      showNoticeModal : false,
+      showConfirmation : false,
     }
   }
 
   componentDidMount () {
+    this.props.setSelectedNavigation(1)
+    this.presenter.getMplTypes()
+    this.presenter.getMplValidate(this.state.loanType)
+    this.presenter.getMplPurposeOfAvailment(
+      this.state.loanType,
+      1,
+      1)
   }
 
+  /* Notice Response*/
+  noticeOfUndertaking (noticeResponse) {
+    this.setState({ showNoticeModal : true, noticeResponse })
+  }
+
+  /* Implementation*/
+
+  showMPLFormAttachments (formAttachments) {
+    this.setState({ formAttachments })
+  }
+
+  showOffset (offset) {
+    this.setState({ offset })
+  }
+
+  showValidate (validateLoanType) {
+    this.setState({ validateLoanType })
+  }
+
+  showPurposeOfAvailment (purposeOfAvailment) {
+    this.setState({ purposeOfAvailment })
+  }
+
+  /*Loader*/
+
+  hideCircularLoader () {
+    this.setState({ enabledLoader : false })
+  }
+
+  showCircularLoader () {
+    this.setState({ enabledLoader : true })
+  }
   /* Navigage back to loans Option*/
   navigate () {
     this.props.history.push('/mybenefits/benefits/loans')
@@ -24,7 +77,17 @@ class ComputerLoanFragment extends BaseMVPView {
 
   render () {
     const {
-    } = this.state
+      purposeOfAvailment,
+      loanType,
+      validateLoanType,
+      offset,
+      enabledLoader,
+      formAttachments,
+      showConfirmation,
+      showNoticeModal,
+      showNoticeResponseModal,
+      noticeResponse,
+      response } = this.state
     return (
       <div>
         <div>
@@ -36,8 +99,20 @@ class ComputerLoanFragment extends BaseMVPView {
             Computer Loan
           </h2>
         </div>
-          <FormComponent
-          />
+          {
+            enabledLoader ?
+             <center className = { 'circular-loader-center' }>
+               <CircularLoader show = { this.state.enabledLoader }/>
+             </center> :
+            <FormComponent
+              loanType = { loanType }
+              purposeOfAvailment = { purposeOfAvailment }
+              validateLoanType = { validateLoanType }
+              formAttachments = { formAttachments }
+              offset = { offset }
+              presenter = { this.presenter }
+            />
+          }
       </div>
     )
   }
