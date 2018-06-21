683fec94458ff002fdf9073d68411c274569e8de
Karen Joy D. Del Rosario <kjddelrosario@core.unionbank.com>
2018-06-21 11:06:49 +0800

    pull to dev

---
+++ src/views/computerloan/modals/ComputerSupplierModal.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import { Modal , GenericButton } from '../../../ub-components/'
+
+import './styles/computerModalStyle.css'
+
+class ComputerSupplierModal extends Component {
+render () {
+  const { onClose, term, onSubmit } = this.props
+
+return (
+  <Modal
+    onClose = { onClose }
+    isDismisable = { true }>
+    <div>
+      <center>
+        <h2>
+          List of Supplier
+        </h2>
+        <h4>
+        </h4>
+      </center>
+    </div>
+    <div>
+      {
+        term && term.map((resp, key) =>
+        <GenericButton
+          className = { 'computer-modal-button' }
+          key = { key }
+          text = {`Term ${resp && resp.term} Rate (${resp && resp.rate}%)` }
+          onClick = { () => onSubmit( resp, false ) }/>
+        )
+      }
+    </div>
+  </Modal>
+    )
+  }
+}
+ComputerSupplierModal.propTypes = {
+  onClose : PropTypes.func,
+  term : PropTypes.array,
+  onSubmit : PropTypes.func
+}
+
+export default ComputerSupplierModal
