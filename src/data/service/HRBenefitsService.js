0fcda203a0c63848d1b7f4ff9da2b1e8e29971f2
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-06-26 16:56:19 +0800

    WIP carlease service and client side of carlease

--- src/data/service/HRBenefitsService.js
+++ src/data/service/HRBenefitsService.js
@@ -347,12 +347,12 @@ export default class HRBenefitsService {
   }
 
   getCarValidate (token) {
-    return this.apiClient.get('v1/employees/lease/cars', {
+    return this.apiClient.get('v1/eligibility/car', {
       headers: { token }
     })
   }
 
-  getCarRequest (
+  addCarRequest (
     token,
     accountToken,
     accountNumber,
@@ -370,13 +370,19 @@ export default class HRBenefitsService {
     formData.append('uuid', 12345)
     formData.append('body', JSON.stringify(addCarleaseObject))
     formData.append('attachments', carRequestParam.attachments)
-    return this.apiClient.post('v1/lease/car/request', carRequestParam, {
+    return this.apiClient.post('v1/leases/car', carRequestParam, {
       headers: { token }
     })
   }
 
-  getCarLease (token) {
-    return this.apiClient.post('v1/leases/car', {
+  addCarLeasePayment (token) {
+    return this.apiClient.post('v1/leases/car/payment', {
+      headers: { token }
+    })
+  }
+
+  addCarLeaseConfirmation (token) {
+    return this.apiClient.post('v1/leases/car/confirm', {
       headers: { token }
     })
   }
