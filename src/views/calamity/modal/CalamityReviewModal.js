1ed40ed14039c97ce2edce305e4478f7eeda44bc
Mark Anthony Parcia <parciamarkanthony@gmail.com>
2018-07-07 10:57:17 +0000

    CalamityReviewModal.js edited online with Bitbucket

--- src/views/calamity/modal/CalamityReviewModal.js
+++ src/views/calamity/modal/CalamityReviewModal.js
@@ -65,7 +65,6 @@ class CalamityReviewModal extends Component {
         backgroundRepeat : 'no-repeat',
       }
     }
-console.log(preferredDate)
     return (
       <Modal
         isDismisable={ isDismisable }
