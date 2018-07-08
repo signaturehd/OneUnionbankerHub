99ab992700cca4e16fe44b5a63a6dd9f6979924f
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-07-08 15:28:41 +0800

    updated validation in utils folder

--- src/utils/validate/RequiredNumberValidation.js
+++ src/utils/validate/RequiredNumberValidation.js
@@ -1,7 +1,12 @@
 export default class RequiredNumberValidation {
 
   isValid (s) {
-    const regex = /[0-9]/
+    const regex = /^[0-9\.]+$/
+    return regex.test(String(s))
+  }
+
+  isValidYear (s) {
+    const regex = /^[0-9]+$/
     return regex.test(String(s))
   }
 }
