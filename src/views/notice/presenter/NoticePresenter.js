d5fda378384b808bde05d43ddacf2cd0e65e9029
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-07-13 19:30:49 +0800

    Updated notice unique for mpl

--- src/views/notice/presenter/NoticePresenter.js
+++ src/views/notice/presenter/NoticePresenter.js
@@ -1,9 +1,12 @@
 import UpdateNoticeInteractor from '../../../domain/interactor/notice/UpdateNoticeInteractor'
+import UpdateNoticeMplInteractor from '../../../domain/interactor/notice/UpdateNoticeMplInteractor'
 import NoticeParam from '../../../domain/param/NoticeParam'
+import NoticeMplParam from '../../../domain/param/NoticeMplParam'
 
 export default class NoticePresenter {
   constructor (container) {
     this.updateNoticeInteractor = new UpdateNoticeInteractor(container.get('HRBenefitsClient'))
+    this.updateNoticeMplInteractor = new UpdateNoticeMplInteractor(container.get('HRBenefitsClient'))
   }
 
   setView (view) {
@@ -13,6 +16,18 @@ export default class NoticePresenter {
   updateNotice (transactionId, isAgree, benefitId) {
     this.view.showLoading()
     this.updateNoticeInteractor.execute(NoticeParam(transactionId, isAgree, benefitId))
+     .subscribe(response => {
+      this.view.hideLoading()
+      this.view.onSuccess(response)
+     }, e => {
+      this.view.hideLoading()
+      // TODO prompt generic error
+     })
+  }
+
+  updateNoticeMpl (transactionId, isAgree, benefitId, spouseAway) {
+    this.view.showLoading()
+    this.updateNoticeMplInteractor.execute(NoticeMplParam(transactionId, isAgree, benefitId, spouseAway))
      .subscribe(response => {
       this.view.hideLoading()
       this.view.onSuccess(response)
