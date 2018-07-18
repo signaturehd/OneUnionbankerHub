d66656f0321a0dd7323e3b2ad5ad6270bfae3208
Carlo <cuevascarlo07@gmail.com>
2018-07-18 15:39:28 +0800

    Updated dental reimbursement

--- src/data/service/HRBenefitsService.js
+++ src/data/service/HRBenefitsService.js
@@ -79,11 +79,11 @@ export default class HRBenefitsService {
       releasingCenter,
       type : dentalReimbursementParam.dependentId.id !== 1 ? 2 : 1,
       procedures : dentalReimbursementParam.procedure,
+      dependentId : dentalReimbursementParam.dependentId.id
     }
 
     formData.append('uuid', 12345)
     formData.append('dentcert1', dentalReimbursementParam.file1)
-    formData.append('dependentId', dentalReimbursementParam.dependentId.id)
     formData.append('dentcert2', dentalReimbursementParam.file2)
     formData.append('body', JSON.stringify(dentalRObject))
     return this.apiClient.post('v2/reimbursements/dental/submit', formData, {
