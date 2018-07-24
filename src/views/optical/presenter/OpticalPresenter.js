46fca09e8b4fc31ca9a26772164b45594c869cc7
Carlo <cuevascarlo07@gmail.com>
2018-07-24 14:43:10 +0800

    adjusted salary loan for mpl

--- src/views/optical/presenter/OpticalPresenter.js
+++ src/views/optical/presenter/OpticalPresenter.js
@@ -5,33 +5,32 @@ import store from '../../../store'
 import { NotifyActions } from '../../../actions'
 
 export default class OpticalPresenter {
- constructor (container) {
-   this.addOpticalInteractior = new AddOpticalInteractor(container.get('HRBenefitsClient'))
-   this.getOpticalInteractor = new GetOpticalInteractor(container.get('HRBenefitsClient'))
- }
+  constructor (container) {
+    this.addOpticalInteractior = new AddOpticalInteractor(container.get('HRBenefitsClient'))
+    this.getOpticalInteractor = new GetOpticalInteractor(container.get('HRBenefitsClient'))
+  }
 
- setView (view) {
-  this.view = view
- }
+  setView (view) {
+    this.view = view
+  }
 
- addOptical (amount, form1, form2) {
-  this.addOpticalInteractior.execute(OpticalParam(amount, form1, form2))
-   .subscribe(optical => {
-    this.view.noticeOfUndertaking(optical)
-    // this.view.showOptical(optical)
-   }, e => {
-    this.view.noticeResponse(e)
-    // TODO prompt generic error
-   })
- }
+  addOptical (amount, form1, form2) {
+    this.addOpticalInteractior.execute(OpticalParam(amount, form1, form2))
+      .subscribe(optical => {
+        this.view.noticeOfUndertaking(optical)
+        // this.view.showOptical(optical)
+      }, e => {
+        this.view.noticeResponse(e)
+        // TODO prompt generic error
+      })
+  }
 
- getOptical () {
-   this.getOpticalInteractor.execute()
-    .subscribe(response => {
-      this.view.isEligible(response)
-    }, e => {
-
-      // TODO notify
-    })
- }
+  getOptical () {
+    this.getOpticalInteractor.execute()
+      .subscribe(response => {
+        this.view.isEligible(response)
+      }, e => {
+        // TODO notify
+      })
+  }
 }
