a04958c9c26e07d9f53c8126b521785e3c832937
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-08-05 16:13:13 +0800

    Updated login view (show and hide of eye)

--- src/views/login/LoginView.js
+++ src/views/login/LoginView.js
@@ -5,6 +5,8 @@ import LoginPresenter from './presenter/LoginPresenter'
 
 import BaseMVPView from '../common/base/BaseMVPView'
 
+import { RequiredValidation } from '../../utils/validate'
+
 import {
   GenericButton,
   Card,
@@ -28,23 +30,34 @@ class LoginView extends BaseMVPView {
     super(props)
 
     this.state = {
-      username: '',
-      password: '',
       showTermsAndCondition : false,
       showOtpModal: false,
       disabled : false,
-      terms : null,
+      username: '',
+      password: '',
       type: 'password',
+      status : 'hide',
+      terms : null,
     }
     this.showHide = this.showHide.bind(this)
     this.onLoginSuccess = this.onLoginSuccess.bind(this)
+    this.proceedToValidation = this.proceedToValidation.bind(this)
   }
    showHide (e) {
     e.preventDefault()
     e.stopPropagation()
-    this.setState({
-      type: this.state.type === 'input' ? 'password' : 'input'
-    })
+
+    if(this.state.status === 'hide') {
+      this.setState({
+        status : 'show',
+        type: this.state.type === 'input' ? 'password' : 'input'
+      })
+    } else if (this.state.status === 'show') {
+      this.setState({
+        status : 'hide',
+        type: this.state.type === 'password' ? 'input' : 'password'
+      })
+    }
   }
 
   componentDidMount () {
@@ -83,10 +96,43 @@ class LoginView extends BaseMVPView {
     window.open('https://play.google.com/store/apps/details?id=com.unionbankph.oneuhub')
   }
 
+  proceedToValidation (user, pass) {
+    if(!new RequiredValidation().isValid(user)) {
+      store.dispatch(NotifyActions.addNotify({
+        title : 'Login Credentials',
+        message : 'Employee ID is required',
+        type: 'warning',
+        duration : 2000,
+      })
+    )
+    } else if (!new RequiredValidation().isValid(pass)) {
+        store.dispatch(NotifyActions.addNotify({
+          title : 'Login Credentials',
+          message : 'Password is required',
+          type: 'warning',
+          duration : 2000,
+        })
+      )
+    }
+    else {
+      this.presenter.login(this.state.username, this.state.password)
+    }
+  }
+
 
   render () {
-    const { showOtpModal, username, terms, showTermsAndCondition } = this.state
+    const {
+      showOtpModal,
+      username,
+      password,
+      terms,
+      showTermsAndCondition,
+      status,
+      disabled,
+      type,
+    } = this.state
     const { notify } = this.props
+
     return (
       <div>
         { super.render() }
@@ -94,7 +140,7 @@ class LoginView extends BaseMVPView {
           // TODO properly show otp modal as 'modal', not by just swapping views lol
           showOtpModal &&
           <OtpModal
-            show = { this.state.showOtpModal }
+            show = { showOtpModal }
             onClose = { () => this.setState({ showOtpModal : false }) }
             parent = { this }
             username = { username }
@@ -115,20 +161,26 @@ class LoginView extends BaseMVPView {
             <br/>
             <GenericInput
               autocomplete='off'
-              onChange = { e => this.setState({ username: e.target.value }) }
+              onChange = { e =>
+                this.setState({ username: e.target.value }) }
               text = { 'Employee ID' }
               type = { 'text' }/>
             <br/>
             <GenericInput
-              autocomplete='off'
-              onChange = { e => this.setState({ password: e.target.value }) }
+              autocomplete = { 'off' }
+              onChange = { e =>
+                this.setState({ password: e.target.value }) }
               text = { 'Password' }
-              type = { this.state.type }
-              className={ 'password__input' }/>
-              <span className={'password__show'} onClick={this.showHide}>{this.state.type === 'input' ? '' : ''}</span>
+              type = { type }
+              className = { 'password__input' }/>
+            <span
+              className = { `password_icon password_${ status }` }
+              onClick = { this.showHide }>
+              { type === 'input' ? '' : ''}
+            </span>
               <br/>
             {
-              this.state.disabled ?
+              disabled ?
               <center>
               <br/>
               <br/>
@@ -140,9 +192,11 @@ class LoginView extends BaseMVPView {
                 <br/>
                 <br/>
                   <GenericButton
-                    disabled = {this.state.disabled}
-                    text="Login"
-                    onClick = { () => this.presenter.login(this.state.username, this.state.password)}/>
+                    disabled = { disabled }
+                    text = { 'LOGIN' }
+                    onClick = { () =>
+                      this.proceedToValidation( username, password )
+                    }/>
                 <br/>
                 <br/>
               </div>
