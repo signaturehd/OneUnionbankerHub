320597a3c0184dca6777b2b642e7c2bf64ad754e
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-08-17 14:36:38 +0800

    Updated existing loans history and summary

--- src/views/existingloans/MyExistingLoansFragment.js
+++ src/views/existingloans/MyExistingLoansFragment.js
@@ -5,15 +5,21 @@ import ConnectPartial from '../../utils/ConnectPartial'
 import BaseMVPView from '../common/base/BaseMVPView'
 import Presenter from './presenter/MyExistingLoansPresenter'
 
-import MyExistingLoansCardComponent from './components/MyExistingLoansCardComponent'
+import ExistingLoansSummaryCardComponent from './components/ExistingLoansSummaryCardComponent'
+import ExistingLoansHistoryCardComponent from './components/ExistingLoansHistoryCardComponent'
 
 import {
   CircularLoader,
   Card,
   GenericButton,
+  Line
 } from '../../ub-components/'
 
+import { format } from '../../utils/numberUtils'
+
 import './styles/myExistingLoanStyle.css'
+
+import moment from 'moment'
 
 class MyExistingLoansFragment extends BaseMVPView {
   constructor (props) {
@@ -50,19 +56,52 @@ class MyExistingLoansFragment extends BaseMVPView {
           </div>
           :
           <div className = { 'existing-loans-grid-container' }>
-          {
-            existingLoans.map((resp, key) => (
-              <MyExistingLoansCardComponent
-                key = { key }
-                description = { resp.description }
-                amortization = { resp.amortization }
-                balance = { resp.balance }
-                promissoryNote = { resp.promissoryNote }
-                date = { resp.date }
+            <div>
+              {
+                existingLoans.map((resp, key) =>
+                <div className = { 'text-align-center' }>
+                  <h2>
+                    &#8369; { format(resp.balance) }
+                  </h2>
+                  <h2>
+                    Outstanding Balance
+                  </h2>
+                  <h2>
+                    { moment(resp.date).format('DD MMM YYYY') }
+                  </h2>
+                </div>
+                )
+              }
+            </div>
+            <div>
+              <div className = { 'existing-loan-summary-grid-x2' }>
+                <div>
+                  Existing Loans Summary
+                </div>
+                <div>
+                  <Line/>
+                </div>
+              </div>
+              <br/>
+              <ExistingLoansSummaryCardComponent
+                existingLoans = { existingLoans }
                 />
-              )
-            )
-          }
+              <br/>
+            </div>
+            <div>
+              <div className = { 'existing-loan-summary-grid-x2' }>
+                <div>
+                  Loans History
+                </div>
+                <div>
+                  <Line/>
+                </div>
+              </div>
+              <br/>
+              <ExistingLoansHistoryCardComponent
+                existingLoans = { existingLoans }
+                />
+            </div>
           </div>
         }
       </div>
