5e032bc02639e0e14083679dba60d5a0ebcda773
Karen Joy D. Del Rosario <kjddelrosario@core.unionbank.com>
2018-08-17 16:34:33 +0800

    Add Transaction details in Maternity Assistance

--- src/views/transaction/components/TransactionMaternityDetailsComponent/MaternityOtherDetailCardComponent.js
+++ src/views/transaction/components/TransactionMaternityDetailsComponent/MaternityOtherDetailCardComponent.js
@@ -16,6 +16,11 @@ class MaternityOtherDetailCardComponent extends Component {
   render () {
   const { detailsMaternity } = this.props
 
+  const amount = TransactionPersonalFunction.checkedAmount(detailsMaternity)
+  const deliveryDate = TransactionPersonalFunction.checkedDeliveryDate(detailsMaternity)
+  const deliveryType = TransactionPersonalFunction.checkedType(detailsMaternity)
+  const recipient = TransactionPersonalFunction.checkedRecipient(detailsMaternity)
+
   return (
     <div  className = { 'transaction-component-otherdetails-form' }>
       <div>
@@ -23,23 +28,66 @@ class MaternityOtherDetailCardComponent extends Component {
           <h2 className = { 'font-weight-bolder' }> Personal Data </h2>
           <br/>
         </div>
+
+
+        <div className = { 'transaction-icons-details-grid' }>
+          <span className = { ' transaction-card-icon-settings' }></span>
+            <div>
+              <h2 className = { 'font-weight-ligter' }>
+                { recipient }
+              </h2>
+              <h2 className = { 'unionbank-color font-size-12px' }>
+                Recipient
+              <br/>
+              <br/>
+              </h2>
+            </div>
+        </div>
+
+        <div className = { 'transaction-icons-details-grid' }>
+          <span className = { ' transaction-card-icon-settings' }></span>
+          <div>
+            <h2 className = { 'font-weight-ligter' }>
+               { deliveryType }
+            </h2>
+            <h2 className = { 'unionbank-color font-size-12px' }>
+              Delivery type
+            <br/>
+            <br/>
+            </h2>
+          </div>
+        </div>
+
         <br/>
       </div>
       <div>
-        <div>
-          <h2 className = { 'font-weight-bolder' }> Certification </h2>
-          <br/>
-        </div>
-        <div>
+        <br/>
+        <br/>
+        <div className = { 'transaction-icons-details-grid' }>
+          <span className = { ' transaction-card-icon-settings' }></span>
           <div>
             <h2 className = { 'font-weight-ligter' }>
-
+               { deliveryDate }
             </h2>
             <h2 className = { 'unionbank-color font-size-12px' }>
-              Procedure
+              Delivery date
+            <br/>
+            <br/>
             </h2>
+          </div>
+        </div>
+
+        <div className = { 'transaction-icons-details-grid' }>
+          <span className = { ' transaction-card-icon-settings' }></span>
+          <div>
+            <h2 className = { 'font-weight-ligter' }>
+               { amount }
+            </h2>
+            <h2 className = { 'unionbank-color font-size-12px' }>
+              Amount
             <br/>
             <br/>
+            </h2>
           </div>
         </div>
         <br/>
