46d74f8dfadc479e183a9c7a5d5ce909b04541d6
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-08-21 19:33:14 +0800

    updated dentalloa labell and spacing (this.navigate() replaced with modal in prompt message)

--- src/views/dentalloa/presenter/DentalLoaPresenter.js
+++ src/views/dentalloa/presenter/DentalLoaPresenter.js
@@ -22,8 +22,8 @@ export default class DentalLoaPresenter {
     .subscribe(response => {
       this.view.hideCircularLoader()
       this.view.getDentalLoa(response)
-    }, e => {
-      this.view.hideCircularLoader()
+    }, error => {
+      this.view.showErrorMessage(error, true)
     })
  }
 
