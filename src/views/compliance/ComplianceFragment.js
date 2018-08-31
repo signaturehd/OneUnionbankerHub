9f2c77cadad278e6ff23c3d98df4a194dc59e190
frankln <frankln.zamora@gmail.com>
2018-08-31 15:37:44 +0800

    mycompliance - added codeofconduct card

--- src/views/compliance/ComplianceFragment.js
+++ src/views/compliance/ComplianceFragment.js
@@ -1,206 +1,95 @@
 import React from 'react'
 import { Switch, Route, createBrowserHistory } from 'react-router-dom'
 import PropTypes from 'prop-types'
+
 import BaseMVPView from '../common/base/BaseMVPView'
 import ConnectView from '../../utils/ConnectView'
 import Presenter from './presenter/CompliancePresenter'
-import ResponseModal from '../notice/NoticeResponseModal'
-import {
-  InputModal,
-  Card,
-  Modal,
-  GenericButton,
-  GenericInput,
-  CircularLoader,
-  FloatingActionButton,
-  Line
- } from '../../ub-components'
 
-import { RequiredNumberValidation } from '../../utils/validate/'
+/*
+import PodcastFragment from '../podcast/PodcastFragment'
+import PodcastPlayerFragment from '../podcastplayer/PodcastPlayerFragment'
+import LibraryView from '../library/LibraryFragment'
+import TrainingFragment from '../trainings/TrainingFragment'
+*/
 
-import './styles/compliancesStyle.css'
+import CodeOfConductFragment from './fragments/codeofconduct/CodeOfConductFragment'
 
+import { InputModal, Card, GenericButton } from '../../ub-components'
 
-import store from '../../store'
-import { NotifyActions } from '../../actions'
+import './styles/mycompliance.css'
 
 class ComplianceFragment extends BaseMVPView {
   constructor (props) {
     super(props)
-    this.state = {
-      enabledLoader : false,
-      compliancesData : null,
-      showEnterPin : false,
-      pin : '',
-      noticeResponse : null,
-      showNoticeResponseModal : false,
-      loadingModal : false,
-    }
   }
 
   componentDidMount () {
     this.props.setSelectedNavigation(9)
-    this.presenter.getCompliancesPdf()
-  }
-
-  onSubmit () {
-    const { pin, showEnterPin } = this.state
-
-    if (pin === null || pin === '') {
-      store.dispatch(NotifyActions.addNotify({
-         title : 'Code of Conduct' ,
-         message : 'Pin is empty',
-         type : 'warning',
-         duration : 2000
-       })
-     )
-   } else if (String(pin).length < 5) {
-      store.dispatch(NotifyActions.addNotify({
-         title : 'Code of Conduct' ,
-         message : 'Please enter your 5-digits code',
-         type : 'warning',
-         duration : 2000
-       })
-     )
-    } else {
-      this.presenter.submitPin(String(pin))
-      this.setState({ showEnterPin : false, pin : '' })
-    }
-  }
-
-  circularLoader (enabledLoader) {
-    this.setState({ enabledLoader })
-  }
-
-  modalLoader (loadingModal) {
-    this.setState({ loadingModal })
-  }
-
-  setCompliancesPdf (compliancesData) {
-    this.setState({ compliancesData })
-  }
-
-  noticeResponse (noticeResponse, showNoticeResponseModal) {
-    this.setState({ noticeResponse, showNoticeResponseModal, showEnterPin : false })
-  }
-
-  navigate () {
-    this.props.history.push('/')
-  }
-
-  validateInputPin (e) {
-    this.setState({ pin : this.inputPin(e) })
-  }
-
-  inputPin (value) {
-    return value && value.replace(/[^0-9]/g, '')
   }
 
   render () {
-    const { history, onClick } = this.props
-    const {
-      compliancesData,
-      enabledLoader,
-      showEnterPin,
-      pin,
-      noticeResponse,
-      showNoticeResponseModal,
-      loadingModal,
-    } = this.state
+    const { history } = this.props
+    const { accountNumber, showAccountNumberModal } = this.state
+    const mycompliance = [{
+      id: 0 ,
+      styleName: 'mycompliance-cards-1 mycompliance-option-default font-weight-bold',
+      title: 'Code of Conduct',
+      path: '/mycompliance/codeofconduct',
+    }]
+
+    const MyCompliance = () => (
+      <div className = { 'mycompliance-container' }>
+        <div>
+          <h2 className={ 'header-margin-default text-align-left' }> My Compliance </h2>
+          <h2> Be an Informed risk-taker. Which reference would you like to read today? </h2>
+        </div>
+        {
+          showAccountNumberModal &&
+            <InputModal
+              isDismisable = { true }
+              onClose = { () => this.setState({ showAccountNumberModal : false }) }
+              onChange = { e => this.setState({ accountNumber: e.target.value }) }
+              placeholder = { 'Account Number' }
+              type = { 'text' }
+              onSubmit = { e => {
+                  e.preventDefault()
+                  this.presenter.validateAccountNumber(accountNumber)
+                }
+              }
+          />
+        }
+        <div className = { 'mycompliance-adjustment' }>
+        <div className = { 'mycompliance-card-container' }>
+          {
+          mycompliance.map((value, idx) => (
+            <Card
+              className = { 'mycompliance-card' }
+              onClick={ () =>
+                history.push(value.path)
+              }
+              key={ idx }>
+              <div className = { 'mycompliance-column-grid' }>
+                <div
+                  className={ value.styleName }
+                  text={ value.title } >
+                </div>
+                <p className={ 'mycompliance-option-cards font-weight-bold' }>{ value.title }</p>
+              </div>
+            </Card>
+          ))
+          }
+        </div>
+      </div>
+    </div>)
 
     return (
       <div>
-      <h2 className = { 'header-margin-default' }>My Compliance</h2>
-      <br/>
-      <br/>
-      {
-        loadingModal &&
-        <Modal>
-          <center>
-            <h3>Please wait while Loading...</h3>
-            <br/>
-            <br/>
-            <CircularLoader show={true}/>
-           </center>
-         </Modal>
-      }
-      {
-        showNoticeResponseModal &&
-        <ResponseModal
-          onClose = { () => {
-            this.setState({ showNoticeResponseModal : false })
-            this.navigate()
-          }}
-          noticeResponse = { noticeResponse }
-        />
-      }
-      {
-        showEnterPin &&
-        <Modal
-          onClose={ () => this.setState({ showEnterPin : false }) }
-          isDismisable={ true }
-          >
-          <div>
-            <p className = { 'pin-label' }>Please enter your registered digital signature (PIN).</p>
-            <GenericInput
-              autocomplete = { 'off' }
-              value = { pin }
-              onChange = { (e) => this.validateInputPin(e.target.value) }
-              text = { 'Password' }
-              type = { 'password' }
-              maxLength = { 5 }
-              inputProps = { 'pin-label' }
-            />
-            <p className={ 'pin-label font-12' }>Please enter your 5-digits code</p>
-            <br/>
-            <GenericButton
-              type = { 'button' }
-              text = { 'Submit' }
-              disabled = { (String(pin).length < 5) }
-              onClick = {
-                () => {
-                  this.onSubmit()
-                }
-              }
-              className={ (String(pin).length < 5) ? 'compliance-disabled' : 'compliance-submit' }
-              />
-          </div>
-        </Modal>
-      }
-      {
-        enabledLoader ?
-        <center className = { 'circular-loader-center' }>
-          <CircularLoader show = { enabledLoader }/>
-        </center> :
-        <div className={ 'compliance-body' }>
-          <div></div>
-          <div>
-              {
-                compliancesData &&
-                compliancesData.map((compliance, key) =>
-                <Card
-                  className = { 'compliance-card-content' }
-                  key = { key } >
-                  <div dangerouslySetInnerHTML = {{ __html : compliance.content }}>
-                  </div>
-                </Card>
-                )
-              }
-            <br/>
-            <div className={ 'compliance-body' }>
-              <div></div>
-              <GenericButton
-                type = { 'button' }
-                text = { 'I Acknowledge' }
-                onClick = {
-                  () => this.setState({ showEnterPin : true })
-                }
-                className={ 'compliance-submit' }
-                />
-            </div>
-          </div>
-        </div>
-      }
+        <Switch>
+          <Route path = '/mycompliance/codeofconduct' render = { props =>
+            <CodeOfConductFragment { ...props } /> } />
+            <Route path = '/mycompliance' render = {  MyCompliance } />
+        </Switch>
       </div>
     )
   }
@@ -208,8 +97,5 @@ class ComplianceFragment extends BaseMVPView {
 
 ComplianceFragment.propTypes = {
   setSelectedNavigation: PropTypes.func,
-  onClick: PropTypes.func,
-  history: PropTypes.object,
 }
-
 export default ConnectView(ComplianceFragment, Presenter)
