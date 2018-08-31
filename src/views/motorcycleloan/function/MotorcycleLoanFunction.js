f315ed8de8a93f470f79a95812e91842276b738e
Karen Joy D. Del Rosario <kjddelrosario@core.unionbank.com>
2018-08-31 14:57:42 +0800

    Added Validation in motorcycle loan

---
+++ src/views/motorcycleloan/function/MotorcycleLoanFunction.js
@@ -0,0 +1,7 @@
+import {
+  RequiredValidation
+} from '../../../utils/validate'
+
+export function checkedValidateInput (input) {
+  return new RequiredValidation().isValid(input) ? true : false
+}
