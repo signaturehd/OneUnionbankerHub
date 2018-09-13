15d6e8f60a23ced326e5b9d2e584691a3bd57504
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-09-13 15:59:11 +0800

    Updated prod version

--- src/views/login/LoginView.js
+++ src/views/login/LoginView.js
@@ -237,7 +237,7 @@ class LoginView extends BaseMVPView {
               <h2
                 onClick = { () => window.open(this.getSupportURL()) }
                 className = { 'unionbank-color font-size-12px text-align-left cursor-pointer' }>Learn More.</h2>
-              <div className={ 'login-version' }>v 4.1.5.4</div>
+              <div className={ 'login-version' }>v 4.1.5.8</div>
             </div>
         </Card>
 
