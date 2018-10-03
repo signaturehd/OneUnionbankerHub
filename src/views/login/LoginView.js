60128abf2e004abd827d43d2fc2a0a9167c27c0b
Parcia <parciamarkanthony@gmail.com>
2018-10-03 08:13:22 +0800

    Updated version for login

--- src/views/login/LoginView.js
+++ src/views/login/LoginView.js
@@ -140,8 +140,8 @@ class LoginView extends BaseMVPView {
     } = this.props
 
     let version = 5
-    let majorVersion = 2
-    let minorVersion = 0
+    let majorVersion = 3
+    let minorVersion = 1
     let versionNumber = version + '.' + majorVersion + '.' + minorVersion
 
     return (
