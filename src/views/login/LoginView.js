936b33899bc446c7ecd97cb9b3db13c6dcfe0556
Carlo <cuevascarlo07@gmail.com>
2018-10-05 16:35:24 +0800

    Adjusted medical outpatient and version number

--- src/views/login/LoginView.js
+++ src/views/login/LoginView.js
@@ -143,7 +143,7 @@ class LoginView extends BaseMVPView {
     let version = 4
     let majorVersion = 3
     let minorVersion = 0
-    let formatVersion = 1
+    let formatVersion = 3
     let versionNumber = version + '.' + majorVersion + '.' + minorVersion + '.' + formatVersion
 
     return (
