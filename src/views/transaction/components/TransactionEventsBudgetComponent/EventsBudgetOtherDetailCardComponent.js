db559f31d9998a97b55dd1ad2b94eaf5b3b419c7
Parcia <parciamarkanthony@gmail.com>
2018-10-21 15:06:50 +0800

    Updated transaction details WIP (in progress attachments upload)

---
+++ src/views/transaction/components/TransactionEventsBudgetComponent/EventsBudgetOtherDetailCardComponent.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import moment from 'moment'
+import { format } from '../../../../utils/numberUtils'
+
+import { Card, GenericButton } from '../../../../ub-components/'
+
+import * as TransactionPersonalFunction from '../../controller/TransactionPersonalFunction'
+
+class EventsBudgetOtherDetailCardComponent extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+
+  return (
+    <div  className = { 'transaction-component-otherdetails-form' }>
+
+    </div>
+    )
+  }
+}
+
+LaptopLeaseOtherDetailCardComponent.propTypes = {
+}
+
+export default EventsBudgetOtherDetailCardComponent
---
+++ src/views/transactiondetails/fragments/EventsBudgetDetailsFragment.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+
+import PropTypes from 'prop-types'
+
+import moment from 'moment'
+import { Card, Button } from '../../../ub-components'
+import './styles/detailsFragment.css'
+
+import * as TransactionDetailsFunction from '../controller/TransactionDetailsFunction'
+
+import LaptopLeaseDetailCardComponent from
+  '../../transaction/components/TransactionLaptopLeaseComponent/LaptopLeaseDetailCardComponent'
+
+class EventsBudgetDetailsFragment extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    const {
+      claimLaptopLease,
+      attachmentsMethod,
+      agreementsMethod,
+      details,
+    } = this.props
+
+    const detailStatus = TransactionDetailsFunction.checkedBenefitStatus(details.status)
+    const benefitType = TransactionDetailsFunction.checkedBenefitType(details.benefitType)
+    const dateFiled = TransactionDetailsFunction.checkedDateFilled(details)
+    const benefitLabel = TransactionDetailsFunction.getBenefitLabelStatus(details.status)
+
+    return (
+      <div className={ 'transaction-details-global-x3' }>
+        <div></div>
+          <Card>
+            <div className={ 'transaction-details-container' }>
+              <div className = { 'transaction-banner transaction-dentalreimbursement' }>
+                <div className={ 'transaction-banner-card' }>
+                  <div className = { 'text-align-left' }>
+                    <h1 className = { 'transaction-details-name font-weight-normal'}>
+                      { benefitType }
+                    </h1>
+                    <div></div>
+                  </div>
+                  <div className={ 'transaction-details-grid-row' }>
+                    <div></div>
+                    <div className = { 'transaction-details-status-grid' }>
+                      <div className =
+                        { `font-weight-bolder grid-global-row-x3 transaction-default-status transaction-details-status-${ detailStatus }` }
+                      >
+                        <div></div>
+                          { benefitLabel }
+                        <div></div>
+                      </div>
+                      <div className = { 'font-size-14px' }></div>
+                    </div>
+                    <div></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br/>
+            <div>
+              <LaptopLeaseDetailCardComponent
+                details = { details }
+                claimLaptopLease = { (transactionId) => claimLaptopLease(transactionId) }
+                onClickAttachments = { (resp) => attachmentsMethod(resp) }
+                onClickAgreements = { (resp) => agreementsMethod(resp) }
+              />
+            </div>
+          </Card>
+        <div></div>
+      </div>
+    )
+  }
+}
+
+export default EventsBudgetDetailsFragment
