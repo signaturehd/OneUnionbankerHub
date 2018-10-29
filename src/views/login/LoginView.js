a6b1bda33aa52238d47a1cf4475178918bfbd59b
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-10-29 13:27:01 +0800

    Updated login version 5.9.14

--- src/views/login/LoginView.js
+++ src/views/login/LoginView.js
@@ -184,7 +184,7 @@ class LoginView extends BaseMVPView {
 
     let version = 5
     let majorVersion = 9
-    let minorVersion = 13
+    let minorVersion = 14
     let versionNumber = version + '.' + majorVersion + '.' + minorVersion
 
     const objectValue = [{
