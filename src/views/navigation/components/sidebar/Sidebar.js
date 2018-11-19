2d77c5f01d076d4362029f3e921c82ed3ece4b30
Parcia <parciamarkanthony@gmail.com>
2018-11-19 18:10:03 +0800

    Pull on dev

--- src/views/navigation/components/sidebar/Sidebar.js
+++ src/views/navigation/components/sidebar/Sidebar.js
@@ -216,12 +216,7 @@ class SideBar extends Component {
               <div>
                 {
                   modules.map((d, idx) =>
-                  d.id !== 1 &&
-                  d.id !== 4 &&
-                  d.id !== 8 &&
-                  d.id !== 9 &&
                   d.id !== 11 &&
-                  d.id !== 13 &&
                   <div key = { idx }>
                     <li
                       className = { `_text-link ${selected === d.id && 'active'}` }
