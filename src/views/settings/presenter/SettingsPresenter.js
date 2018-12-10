415343fda81fdc004c1847696f31bf05090e37d7
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-12-10 16:25:42 +0800

    Revert throw exception message  catcher

--- src/views/settings/presenter/SettingsPresenter.js
+++ src/views/settings/presenter/SettingsPresenter.js
@@ -62,13 +62,12 @@ export default class SettingsPresenter {
          console.log(e)
        }
      }, e => {
-        this.view.hideLoading()
-        this.view.showProfileBackground('')
-        this.view.showProfile('')
-        this.view.showRank('')
-        this.view.showLineManager('')
-        this.view.showProfileDependent('')
-        this.view.showAccountNumber('')
+       this.view.showProfileBackground(e.message)
+       this.view.showProfile(e.message.employee)
+       this.view.showRank(e.message.rank)
+       this.view.showLineManager(e.message.employee.lineManager)
+       this.view.showProfileDependent(e.message.dependents)
+       this.view.showAccountNumber(e.message.accountNumber)
       // TODO prompt generic error
     })
    }
