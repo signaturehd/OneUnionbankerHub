9d32940e057c67e1e318e2a39ed52c537e227c37
Parcia <parciamarkanthony@gmail.com>
2018-12-30 20:07:22 +0800

    WIP identifying the type of platform in viewing of promotion view  new layoutr

--- src/index.js
+++ src/index.js
@@ -18,10 +18,32 @@ import AppModule from './di/AppModule'
 /* Mobile View */
 import MobileView from './views/mobileplatform/MobileView'
 
+let platformChecker
+const userAgentTest = window.navigator.userAgent
+
+if(userAgentTest.toLowerCase().indexOf('android') !== -1) {
+  platformChecker = true
+} else if (userAgentTest.toLowerCase().indexOf('iphone') !== -1) {
+  platformChecker = true
+} else if (userAgentTest.toLowerCase().indexOf('ipad') !== -1) {
+  platformChecker = true
+} else if (userAgentTest.toLowerCase().indexOf('playbook') !== -1) {
+  platformChecker = true
+} else {
+  platformChecker = false
+}
+
+console.log(userAgentTest)
 
 ReactDOM.render(
   <Provider store={ store }>
     <HashRouter basename = { '/' }>
+    {
+      platformChecker ?
       <MobileView />
+      :
+      <App />
+    }
     </HashRouter>
-  </Provider>, document.getElementById('root'))
+  </Provider>, document.getElementById('root')
+)
