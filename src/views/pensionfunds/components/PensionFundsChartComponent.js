8eb70188ad7fa04dea019f6129cec8d4328551c9
Parcia <parciamarkanthony@gmail.com>
2019-01-12 21:22:19 +0800

    WIP charts css

--- src/views/pensionfunds/components/PensionFundsChartComponent.js
+++ src/views/pensionfunds/components/PensionFundsChartComponent.js
@@ -9,33 +9,6 @@ import {
 } from '../../../ub-components/'
 
 import './styles/fundsComponentStyle.css'
-let data = {
-    labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    datasets: [
-    {
-        label: "Prime and Fibonacci",
-        fillColor: "rgba(220,220,220,0.2)",
-        strokeColor: "rgba(220,220,220,1)",
-        pointColor: "rgba(220,220,220,1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(220,220,220,1)",
-        data: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
-    },
-    {
-        label: "My Second dataset",
-        fillColor: "rgba(151,187,205,0.2)",
-        strokeColor: "rgba(151,187,205,1)",
-        pointColor: "rgba(151,187,205,1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(151,187,205,1)",
-        data: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-    }
-  ]
-}
-
-console.log(data)
 
 class PensionFundsChartComponent extends Component {
   constructor (props) {
@@ -43,9 +16,32 @@ class PensionFundsChartComponent extends Component {
   }
 
   render () {
+    let data = {
+        labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        datasets: [
+        {
+          borderColor: 'white',
+          label: "Prime and Fibonacci",
+          fillColor: "white",
+          strokeColor: "white",
+          pointColor: "white",
+          pointStrokeColor: "white",
+          pointHighlightFill: "white",
+          backgroundColor: "rgb(255,102,0)",
+          pointHighlightStroke: "white",
+          data: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+        },
+      ]
+    }
     return (
-      <div>
-        <LineChart data = { data }/>
+      <div className = { 'pension-chart-grid-3x' }>
+        <div></div>
+        <div>
+          <LineChart
+            width = { 'auto' }
+            data = { data }/>
+        </div>
+        <div></div>
       </div>
     )
   }
