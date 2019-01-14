b7f845400dc45a25045ad263ee51d16ef7016909
masparcia <parciamarkanthony@gmail.com>
2019-01-14 14:38:38 +0800

    Updated View component

--- src/views/celebratedna/CelebrateDNAFragment.js
+++ src/views/celebratedna/CelebrateDNAFragment.js
@@ -12,99 +12,69 @@ import './style/RewardStyle.css'
 import staticImage   from '../../images/DNA.png'
 
 class CelebrateDNAFragment extends BaseMVPView {
-      constructor (props) {
-        super(props)
-        this.state = {
-          rewardDNA: []
-        }
-      }
-
-      componentDidMount () {
-        this.props.setSelectedNavigation(9)
-        this.presenter.getRewardDNA(2)
-      }
-
-       setRewardDNA (rewardDNA) {
-        this.setState({ rewardDNA })
-      }
-
-
-
-      // getRewardsDNAMoment (rewardDNA) {
-      //   this.setState({ rewardDNA })
-      //   this.presenter.getRewardsDNA()
-      // }
-
-      render () {
-        const { history, onClick } = this.props
-
-          const { accountNumber, showAccountNumberModal, rewardDNA } = this.state
-          console.log(rewardDNA)
-          return (
-<<<<<<< HEAD
-            <div className = {'celebrate-container'}>
-
-              <div className={'text-align-center'}>
-
-                <img src={staticImage} className = {''}/>
-
-              </div>
-
-              <div className={'text-align-center'}>
-
-                <h1 >Celebrating a DNA Moment</h1>
-
-              </div>
-
-                  <h6 className={ 'celebrate-container-space' }> This award is given to individuals or teams who demonstrate behaviors aligned to the following: </h6>
-
-                  <h3 className={ 'celebrate-container-space ' }>Distinguishing beliefs/principle:</h3>
-
-                  <p className={'font-size-12px'}>Forwird-thinking, Agile, Open, and Innovative</p>
-
-                  <h3>Values : </h3>
-
-                  <p className={'font-size-12px'}>Integrity, Magis,Ubuntu</p>
-
-                  <GenericInput hint={'Search employees name'}>
-
-                  </GenericInput>
-
-                    <div>
-
-                    <h2 className={'celebrate-container-space'}>Who will I award this to?</h2>
-
-                    </div>
-
-                  <div className={ 'celebrate-container-space' }>
-                      <p>Write a personal message of gratitude for this award`s recipients.</p>
-
-                      <GenericInput  type={ 'textarea' } resize={150}>
-
-                      </GenericInput>
-
-                  </div>
-
-                    <div className={'text-align-center'}>
-
-                      <GenericButton text={ 'Submit' }  className={ 'celebrate-button-center ' } >
-
-                      </GenericButton>
-
-                    </div>
-=======
-            <div>
-                  <h1>Celebrate DNA</h1>
-                  <p>{ rewardDNA.award }</p>
-                  <p>{ rewardDNA.points }</p>
-                  <GenericButton text={'Button'} onclick={''}>
-                  </GenericButton>
->>>>>>> 7f5b6a5866f948a931be7527e3a43a3404ed6526
-            </div>
-
-          )
-        }
+  constructor (props) {
+    super(props)
+    this.state = {
+      rewardDNA: []
     }
+  }
+
+  componentDidMount () {
+    this.props.setSelectedNavigation(9)
+    this.presenter.getRewardDNA(2)
+  }
+
+   setRewardDNA (rewardDNA) {
+    this.setState({ rewardDNA })
+  }
+  // getRewardsDNAMoment (rewardDNA) {
+  //   this.setState({ rewardDNA })
+  //   this.presenter.getRewardsDNA()
+  // }
+
+  render () {
+    const { history, onClick } = this.props
+
+    const { accountNumber, showAccountNumberModal, rewardDNA } = this.state
+    return (
+      <div className = {'celebrate-container'}>
+        <div className={'text-align-center'}>
+          <img src={staticImage} className = {''}/>
+        </div>
+        <div className={'text-align-center'}>
+          <h1 >Celebrating a DNA Moment</h1>
+        </div>
+          <h6 className={ 'celebrate-container-space' }> This award is given to individuals or teams who demonstrate behaviors aligned to the following: </h6>
+          <h3 className={ 'celebrate-container-space ' }>Distinguishing beliefs/principle:</h3>
+          <p className={'font-size-12px'}>Forwird-thinking, Agile, Open, and Innovative</p>
+          <h3>Values : </h3>
+          <p className={'font-size-12px'}>Integrity, Magis,Ubuntu</p>
+          <GenericInput hint={'Search employees name'}>
+          </GenericInput>
+            <div>
+            <h2 className={'celebrate-container-space'}>Who will I award this to?</h2>
+            </div>
+          <div className={ 'celebrate-container-space' }>
+            <p>Write a personal message of gratitude for this award's recipients.</p>
+            <GenericInput  type={ 'textarea' } resize={150}>
+            </GenericInput>
+          </div>
+            <div className={'text-align-center'}>
+            <GenericButton
+              text={ 'Submit' }
+              className={ 'celebrate-button-center ' } />
+        </div>
+        <div>
+          <h1>Celebrate DNA</h1>
+          <p>{ rewardDNA.award }</p>
+          <p>{ rewardDNA.points }</p>
+          <GenericButton text={'Button'} onclick={''}>
+          </GenericButton>
+        </div>
+      </div>
+    )
+  }
+}
 
 CelebrateDNAFragment.propTypes = {
 	setSelectedNavigation: PropTypes.func,
