7496c86e6508b16530e3917fe892b953cead6dbf
unknown <danielleconquilla@bitbucket.org>
2019-01-24 00:38:44 +0800

    removed static token

--- src/data/provider/SessionProvider.js
+++ src/data/provider/SessionProvider.js
@@ -18,7 +18,7 @@ export default class SessionProvider {
   }
 
   getToken () {
-    return this.storage.getItem(TOKEN) || 'employee28'
+    return this.storage.getItem(TOKEN) || ''
   }
 
   setInitialToken (token = '') {
