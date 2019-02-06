b3b25d8cf7a379bf8ffe34786d49a46803fce222
masparcia <parciamarkanthony@gmail.com>
2019-02-06 16:11:35 +0800

    Updated phenom getting of data images in phenom

--- src/domain/interactor/phenom/GetPhenomDetailsInteractor.js
+++ src/domain/interactor/phenom/GetPhenomDetailsInteractor.js
@@ -1,8 +1,31 @@
+import { Observable } from 'rxjs'
+
 export default class GetPhenomDetailsInteractor {
   constructor (client) {
     this.client = client
   }
   execute (id) {
-    return this.client.getPhenomSelectedDiscounts(this.client.getToken(), id)
+    return Observable.create(emitter => {
+      this.client.getPhenomSelectedDiscounts(this.client.getToken(), id)
+      // .flatMap(phenoms => Observable.from(phenoms))
+      .flatMap(phenom => Observable.zip(
+        this.client.getPhenomImage(this.client.getToken(), phenom.rewardImage),
+        this.client.getVendorImage(this.client.getToken(), phenom.rewardLogo),
+        (rewardImageBlob, imageBlob) => {
+          console.log(phenom)
+          const updatedPhenom = phenom
+          updatedPhenom.rewardImage = rewardImageBlob
+          updatedPhenom.rewardLogo = imageBlob
+          return updatedPhenom
+        }))
+        .subscribe(phenom => emitter.next(phenom),
+         e => {
+           emitter.complete(e)
+         },
+         e => {
+          emitter.error()
+        }
+      )
+    })
   }
 }
