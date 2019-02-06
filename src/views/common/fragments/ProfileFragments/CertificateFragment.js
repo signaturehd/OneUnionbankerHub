3f873496af93aba33f75b8a6bc69afc2ac41c13e
masparcia <parciamarkanthony@gmail.com>
2019-02-06 15:18:16 +0800

    Updated profile common view more of informations

--- src/views/common/fragments/ProfileFragments/CertificateFragment.js
+++ src/views/common/fragments/ProfileFragments/CertificateFragment.js
@@ -11,13 +11,24 @@ class CertificateFragment extends Component {
 
   constructor (props) {
     super(props)
+    this.state = {
+      index : 3,
+      viewMoreText: 'view more, work & experience',
+    }
   }
 
   render () {
     const {
+      index,
+      viewMoreText
+    } = this.state
+
+    const {
       onClick,
       profileCertificate,
     }=this.props
+
+    const isVisible = (profileCertificate && profileCertificate.length > 4) ? '' : 'hide'
 
     return (
       <div>
@@ -27,29 +38,47 @@ class CertificateFragment extends Component {
         <br/>
         {
           profileCertificate ?
-            profileCertificate.map((certificate, key)=>
-              <Card
-                key={ key }
-                className={ 'components-view' }>
-                <div>
-                  <img
-                    className={ 'components-image' }
+            <div>
+              {
+                profileCertificate.slice(0, index).map((certificate, key)=>
+                  <Card
                     key={ key }
-                    src={ defaultImage }/>
-                </div>
-                <div>
-                  <h2 className={ 'font-size-18px font-weight-normal' }>
-                    Title : { certificate.name }
-                  </h2>
-                  <h3 className={ 'font-size-17px font-weight-lighter' }>
-                    Issued body: { certificate.issuingBody }
-                  </h3>
-                  <h4 className={ 'font-size-14px font-weight-lighter' }>
-                    Date Issued: { certificate.dateIssued }
-                  </h4>
-                </div>
-              </Card>
-            )
+                    className={ 'components-view' }>
+                    <div>
+                      <img
+                        className={ 'components-image' }
+                        key={ key }
+                        src={ defaultImage }/>
+                    </div>
+                    <div>
+                      <h2 className={ 'font-size-18px font-weight-normal' }>
+                        Title : { certificate.name }
+                      </h2>
+                      <h3 className={ 'font-size-17px font-weight-lighter' }>
+                        Issued body: { certificate.issuingBody }
+                      </h3>
+                      <h4 className={ 'font-size-14px font-weight-lighter' }>
+                        Date Issued: { certificate.dateIssued }
+                      </h4>
+                    </div>
+                  </Card>
+                )
+              }
+              <button
+                type = { 'button' }
+                className = { `viewmore tooltip ${isVisible}` }
+                onClick = {
+                  () => {
+                    if(index === profileCertificate.length)
+                      this.setState({ index : 3, viewMoreText : 'view less, work & experience' })
+                    else
+                      this.setState({ index : profileCertificate.length, viewMoreText : 'view more, work & experience' })
+                  }
+                }>
+                <img src={ require('../../../../images/icons/horizontal.png') } />
+                <span className={ 'tooltiptext' }>{ viewMoreText }</span>
+              </button>
+            </div>
             :
             <center><h3>No Information Available</h3><br/></center>
 
