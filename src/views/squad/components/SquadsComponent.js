702adc464c0355f7cb3d13c319b0a67953d3b43f
Carlo L. Cuevas <carlol.cuevas@Carlos-MacBook-Pro.local>
2019-02-11 12:31:53 +0800

    added squad component

---
+++ src/views/squad/components/SquadsComponent.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+import PropTypes from 'prop-types'
+
+class SquadsComponent extends Component {
+  constructor (props) {
+    super (props)
+  }
+
+  render () {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+SquadsComponent.propTypes = {
+
+}
+
+SquadComponent.defaultProps = {
+  
+}
+
+export default SquadsComponent
