9e538d2cad7ee07e97f0d32de15c67845449d600
masparcia <parciamarkanthony@gmail.com>
2019-02-18 15:16:26 +0800

    Updated version

--- src/views/login/LoginView.js
+++ src/views/login/LoginView.js
@@ -272,7 +272,7 @@ class LoginView extends BaseMVPView {
 
     let version = 8
     let majorVersion = 2
-    let minorVersion = 0
+    let minorVersion = 1
     let versionNumber = version + '.' + majorVersion + '.' + minorVersion
 
     const objectValue = [{
