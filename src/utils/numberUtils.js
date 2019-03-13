5f57231826b709f12efc5a423924adf94d607da5
masparcia <parciamarkanthony@gmail.com>
2019-03-13 13:46:59 +0800

    Updated optical reimbursement amount input, validate input amount and login unlock layou change

--- src/utils/numberUtils.js
+++ src/utils/numberUtils.js
@@ -1,4 +1,19 @@
 export function format (n = 0) {
- var parts = parseFloat(n).toFixed(2).split(".")
- return parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (parts[1] ? "." + parts[1] : "")
+  var parts = parseFloat(n).toFixed(2).split(".")
+  return parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (parts[1] ? "." + parts[1] : "")
+}
+
+export function formatInput (n) {
+  let parts = n && n.replace(/,/g, "").replace(/,/g, "")
+  let value = numberWithCommas(parts)
+  return value
+}
+
+export function formatValue (n) {
+  let parts =  n && n.replace(/,/g, "").replace(/,/g, "")
+  return parts
+}
+
+export function numberWithCommas(x) {
+   return x && x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
